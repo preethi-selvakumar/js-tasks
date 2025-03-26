4042c2046df921046602ec8ec5adacf090cc6739
preethi-selvakumar <preethicoder90@outlook.com>
2025-03-26 21:55:36 +0530

    Added day2 folder

---
+++ day 2/mini project 1/assets/js/script.js
@@ -0,0 +1,13 @@
+// Step 1: Store user details
+let citizen = "India"; 
+let age = 17; 
+
+// Step 2: Check both conditions using logical and comparison operators
+// Condition 1: Age should be 18 or above (age >= 18)
+// Condition 2: Citizenship should be India (citizen === "India")
+let isEligible = (citizen === "India" && age >= 18) ? " Eligible to Vote" : " Not Eligible to Vote";
+
+// Step 3: Print the result based on the conditions
+console.log(isEligible); // Step 4: Output should be displayed in the console
+
+
---
+++ day 2/mini project 2/assets/script.js
@@ -0,0 +1,15 @@
+// 1st Store the temperature in Celsius
+let celsius = 25; 
+
+// 2nd Convert Celsius to Fahrenheit 
+let fahrenheit = (celsius * 9 / 5) + 32; 
+
+// 3rd  Store the converted Fahrenheit value in a variable
+let convertedTemp = fahrenheit; 
+
+// 4th  Display the result in the console 
+console.log("Temperature Converter");
+console.log("*************************");
+console.log("Celsius:", celsius, "°C");
+console.log("Fahrenheit:", convertedTemp, "°F");
+console.log("*************************");
